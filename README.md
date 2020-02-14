34395342043a0ec4c953280d892012be26d2958b
Dana Marcuse <dana@marcuse.us>
2020-02-14 17:49:11 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# JamesonMemorialBets
+
+Predicting Elite: Dangerous market prices with the power of machine learning™
+
+## Data sources
+
+[Live market data from EDDN](https://github.com/edsm-net/eddn/wiki#trading-tool-developers-data-consumers)
+[Mass data dumps from EDDB](https://eddb.io/api)
+[Mass data dumps from EDSM](https://www.edsm.net/en/nightly-dumps)
+
+## Reference material
+
+[Elite: Dangerous player journal manual](https://hosting.zaonce.net/community/journal/v25/Journal-Manual-v25.pdf)
---
+++ market_data_collector.py
@@ -0,0 +1,54 @@
+import zlib
+import zmq
+import simplejson
+import time
+
+relayURI = "tcp://eddn.edcd.io:9500"
+outputFile = "data/commodities.jsonl"
+
+
+def should_save(event) -> bool:
+    schema = event["$schemaRef"]
+    if schema == "https://eddn.edcd.io/schemas/commodity/3":
+        return True
+    elif schema == "https://eddn.edcd.io/schemas/journal/1":
+        return event["message"]["event"] == "Docked"
+
+
+def main():
+    # initialize zeromq subscriber
+    context = zmq.Context()
+    subscriber = context.socket(zmq.SUB)
+
+    # subscribe to all events
+    subscriber.setsockopt(zmq.SUBSCRIBE, b"")
+    subscriber.setsockopt(zmq.RCVTIMEO, 600000)
+
+    file = open(outputFile, "a")
+    events = 0
+
+    while True:
+        try:
+            subscriber.connect(relayURI)
+
+            while True:
+                msg = subscriber.recv()
+
+                if not msg:
+                    subscriber.disconnect(relayURI)
+                    break
+
+                msg = zlib.decompress(msg)
+                json = simplejson.loads(msg)
+                if should_save(json):
+                    file.write(msg.decode("utf8") + "\n")
+                    events += 1
+                    print(f"Received {events} events", end="\r")
+        except zmq.ZMQError as e:
+            print(f"ZMQ error: {e}")
+            subscriber.disconnect(relayURI)
+            time.sleep(5)
+
+
+if __name__ == "__main__":
+    main()
