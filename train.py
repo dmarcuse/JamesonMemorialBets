daf0ccd49257fbda3d45dc7343b11dd65b4601ff
Dana Marcuse <dana@marcuse.us>
2020-03-06 13:21:12 -0500

    Add model saving/loading and evaluation against random predictions

--- train.py
+++ train.py
@@ -2,6 +2,8 @@ import pandas as pd
 from sklearn.metrics import r2_score
 from sklearn.model_selection import train_test_split
 from tensorflow import keras
+from os import path
+from random import random
 
 # How many epochs to train.
 NUM_EPOCHS = 20
@@ -12,18 +14,11 @@ VALIDATION_SPLIT = 0.2
 # The name of the target feature.
 TARGET_FEATURE = "sell_factor"
 
+# Path to save/load model from.
+MODEL_FILE = "bets.tf"
 
-def main():
-    # Load and clean data.
-    data = pd.read_csv("data/cleaned_data.csv")
 
-    # Select target and feature columns.
-    y = data[TARGET_FEATURE]
-    x = data.drop(TARGET_FEATURE, axis=1)
-
-    # Split into training and testing set.
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=VALIDATION_SPLIT)
-
+def train_model(x_train, y_train, x_test, y_test):
     # Create the model.
     model = keras.Sequential([
         keras.layers.Dense(x_train.shape[1], input_shape=(x_train.shape[1],)),  # Input layer
@@ -44,9 +39,32 @@ def main():
         validation_data=(x_test, y_test)
     )
 
+    return model
+
+
+def main():
+    # Load and clean data.
+    data = pd.read_csv("data/cleaned_data.csv")
+
+    # Select target and feature columns.
+    y = data[TARGET_FEATURE]
+    x = data.drop(TARGET_FEATURE, axis=1)
+
+    # Split into training and testing set.
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=VALIDATION_SPLIT)
+
+    # Load or train model
+    if path.exists(MODEL_FILE):
+        model = keras.models.load_model(MODEL_FILE)
+        print(model.summary())
+    else:
+        model = train_model(x_train, y_train, x_test, y_test)
+        keras.models.save_model(model, MODEL_FILE)
+
     # Score the model.
-    print(f"r2 score (training): {r2_score(y_train, model.predict(x_train))}")
-    print(f"r2 score (testing): {r2_score(y_test, model.predict(x_test))}")
+    print(f"r² score (training): {r2_score(y_train, model.predict(x_train))}")
+    print(f"r² score (testing): {r2_score(y_test, model.predict(x_test))}")
+    print(f"r² score (random predictions): {r2_score(y, [random() * 5 for _ in y])}")
 
 
 if __name__ == "__main__":
