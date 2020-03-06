4b39978c8e8ec8470153cd2a6eb360b519a658c1
Garrett Powell <garrett@gpowell.net>
2020-03-06 11:36:13 -0500

    Use train-test split instead of cross-validation for scoring the model
    Cross validation took waaaaaaaay too long. Ain't nobody got time for that.

--- train.py
+++ train.py
@@ -1,43 +1,50 @@
 import pandas as pd
-from sklearn.model_selection import cross_val_score, KFold
+from sklearn.metrics import r2_score
+from sklearn.model_selection import train_test_split
 from tensorflow import keras
-from tensorflow.keras.wrappers.scikit_learn import KerasRegressor
 
+# How many epochs to train.
+NUM_EPOCHS = 20
 
-def build_network(input_size: int):
-    # create model
-    model = keras.Sequential([
-        # input layer
-        keras.layers.Dense(
-            input_size,
-            input_shape=(input_size,),
-            activation="sigmoid"
-        ),
-        keras.layers.Dense(64, activation="relu"),  # hidden layer
-        keras.layers.Dense(1),  # output layer
-    ])
-    model.compile(optimizer="adam", loss="mean_squared_error")
-    print(model.summary())
-    return model
+# How much of the training data to use for testing (0.2 = 20%).
+VALIDATION_SPLIT = 0.2
+
+# The name of the target feature.
+TARGET_FEATURE = "sell_difference"
 
 
 def main():
-    # load and clean data
+    # Load and clean data.
     data = pd.read_csv("data/cleaned_data.csv")
 
-    # select target and feature columns
-    y = data["sell_factor"]
-    x = data.drop("sell_factor", axis=1)
+    # Select target and feature columns.
+    y = data[TARGET_FEATURE]
+    x = data.drop(TARGET_FEATURE, axis=1)
 
-    estimator = KerasRegressor(
-        build_fn=lambda: build_network(x.shape[1]),
-        epochs=20,
-        batch_size=5,
+    # Split into training and testing set.
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=VALIDATION_SPLIT)
+
+    # Create the model.
+    model = keras.Sequential([
+        keras.layers.Dense(x_train.shape[1], input_shape=(x_train.shape[1],)),  # Input layer
+        keras.layers.Dense(64, activation="relu"),  # Hidden layer
+        keras.layers.Dense(64, activation="relu"),  # Hidden layer
+        keras.layers.Dense(1),  # Output layer
+    ])
+    model.compile(optimizer="adam", loss="mean_squared_error")
+    print(model.summary())
+
+    # Train the model.
+    model.fit(
+        x_train,
+        y_train,
+        epochs=NUM_EPOCHS,
         verbose=1
     )
-    kfold = KFold(n_splits=5, shuffle=True)
-    results = cross_val_score(estimator, x, y, scoring="r2", n_jobs=1, cv=kfold)
-    print(f"r^2 Score: {results.mean()}")
+
+    # Score the model.
+    print(f"r2 score (training): {r2_score(y_train, model.predict(x_train))}")
+    print(f"r2 score (testing): {r2_score(y_test, model.predict(x_test))}")
 
 
 if __name__ == "__main__":
